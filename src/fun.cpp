b85867b2628bbe9b3209fdbf3e18208249a5b83f
youngtr4der <105950185+youngtr4der@users.noreply.github.com>
2023-03-15 15:09:19 +0300

    realised fun

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,5 +2,11 @@
 #include "fun.h"
 
 int64_t power(int64_t x, uint16_t n) {
-    return 0;
+    int64_t res = 1;
+
+    for (int64_t i = 0; i < n; i++) {
+        res = res * x;
+    }
+
+    return res;
 }
